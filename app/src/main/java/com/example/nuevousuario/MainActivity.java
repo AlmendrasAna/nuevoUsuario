1ed8b186d46de075f0a9cf099a77f310109824b9
Ana <almendras451@gmail.com>
2023-06-27 20:35:53 -0400

    ejercicio 10 binding

--- app/src/main/java/com/example/nuevousuario/MainActivity.java
+++ app/src/main/java/com/example/nuevousuario/MainActivity.java
@@ -8,34 +8,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.nuevousuario.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+binding = ActivityMainBinding.inflate(getLayoutInflater());
+setContentView(binding.getRoot());
 
-        EditText nameEdit = findViewById(R.id.nameTxt);
-        EditText lastNameEdit = findViewById(R.id.lastnameTxt);
-        EditText emailEdit = findViewById(R.id.emailTxt);
-        EditText passworEdit = findViewById(R.id.passwordTxt);
-
-        Button commitB = findViewById(R.id.buttonUser);
-commitB.setOnClickListener(new View.OnClickListener() {
+binding.buttonUser.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        if(nameEdit.getText().toString().isEmpty() || lastNameEdit.getText().toString().isEmpty()
-                || emailEdit.getText().toString().isEmpty() || passworEdit.getText().toString().isEmpty()){
+        if(binding.nameTxt.getText().toString().isEmpty() || binding.lastnameTxt.getText().toString().isEmpty()
+                || binding.emailTxt.getText().toString().isEmpty() || binding.passwordTxt.getText().toString().isEmpty()){
             Toast.makeText(getBaseContext(), "faltan campos por completar ", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(!emailEdit.getText().toString().contains("@")){
+        if(!binding.emailTxt.getText().toString().contains("@")){
             Toast.makeText(getBaseContext(), "correo invalido ", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        String mensaje= "Bienvenido nuevo usuario " + nameEdit.getText().toString()+ " " + lastNameEdit.getText().toString()
-                + " Email " +emailEdit.getText().toString()+" contraseña "+ passworEdit.getText().toString();
+        String mensaje= "Bienvenido nuevo usuario " + binding.nameTxt.getText().toString()+ " " + binding.lastnameTxt.getText().toString()
+                + " Email " +binding.emailTxt.getText().toString()+" contraseña "+ binding.passwordTxt.getText().toString();
 
         Toast.makeText(getBaseContext(), mensaje, Toast.LENGTH_LONG).show();
 
